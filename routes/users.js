4957b018c9443a1ffb8e2cd106cf358768f2fed6
remi <remi.grall@gmail.com>
2019-11-20 15:50:18 +0100

    Pour pierrick 20/11/2019 15:50

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var cors = require('cors');
 
 
 /* GET users listing. */
+router.use(cors());
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 router.get('/', function(req, res, next) {
   //res.send({id:1});
   var mysql = require('mysql');
-
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   console.log('Get connection ...');
 
   var conn = mysql.createConnection({
@@ -24,14 +33,13 @@ router.get('/', function(req, res, next) {
     conn.query(req1, function(err,rows,fields){
       if (err) throw err;
       console.log("L'utilisateur est : ", rows[0])
+      res.send(rows[0])
     });
 
     var req2 = "INSERT INTO user (login,password,prenom,nom,email) VALUE('pierrick','pierrick','pierrick','pierrick','pierrick')";
     conn.query(req2, function(err,fields){
       if (err) throw err;
     });
-
-
   });
 });
 
