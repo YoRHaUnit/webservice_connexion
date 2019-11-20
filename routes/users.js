f564769ce7b275328dd3e01fe5995695f9c7756e
remi <remi.grall@gmail.com>
2019-11-20 10:08:15 +0100

    Commit number 1 select and insert good

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next) {
+  //res.send({id:1});
+  var mysql = require('mysql');
+
+  console.log('Get connection ...');
+
+  var conn = mysql.createConnection({
+    database: 'projetwebservicesconnexion',
+    host: "localhost",
+    user: "root",
+    password: ""
+  });
+
+  conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var req1 = "select * from user";
+    conn.query(req1, function(err,rows,fields){
+      if (err) throw err;
+      console.log("L'utilisateur est : ", rows[0])
+    });
+
+    var req2 = "INSERT INTO user (login,password,prenom,nom,email) VALUE('pierrick','pierrick','pierrick','pierrick','pierrick')";
+    conn.query(req2, function(err,fields){
+      if (err) throw err;
+    });
+
+
+  });
 });
 
 module.exports = router;
