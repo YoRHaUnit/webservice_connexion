a62be28af87bbb91124541aac7f3bd44c2c9497f
Pierrick <pierrickblot243@gmail.com>
2019-12-07 17:40:33 +0100

    Cors modify

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,11 @@ router.use(express.json())
 router.use(bodyParser.json());
 router.use(express.urlencoded({ extended: true }))
 
-router.use(function(req, res, next) {
+/*router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
 
 
 
