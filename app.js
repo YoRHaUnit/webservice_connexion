aec5fa93d3b2773c78b206c6450e7ca211da5091
K1rby <quentin.fontenay@hotmail.com>
2020-01-13 13:53:49 +0100

    ajout du swagger

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
 const port = 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const db = require('./routes/queries');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(
@@ -14,8 +17,8 @@ app.use(
 );
 
 app.get('/users', db.getUsers);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 app.post('/users', db.createUser);
-//app.get('/users?', db.getUserById());
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 });
